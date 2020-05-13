b95c7876f0b3310721784d1dc57339d5e8d6d6a4
Sorrab <ramonpbarros@gmail.com>
2020-05-12 20:53:15 -0700

    added navbar and favicon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 function Navbar({ currentPage, handlePageChange }) {
-  const pageNames = ["Home", "About", "Blog", "Contact"];
+  const pageNames = ["Home", "About", "Work", "Contact"];
 
   const renderNavTab = (page) => {
     const href = `#${page.toLowerCase()}`;
-    const className = currentPage === page ? "nav-link active" : "nav-link";
+    const className = currentPage === page ? "nav-link" : "nav-link";
     const onClick = () => handlePageChange(page);
 
     return (
-      <li className="nav-item" key={page}>
+      <li className="nav-item active" key={page}>
         <a href={href} onClick={onClick} className={className}>
           {page}
+          <span class="sr-only">(current)</span>
         </a>
       </li>
     );
@@ -33,32 +34,12 @@ function Navbar({ currentPage, handlePageChange }) {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
               <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="index.html">
-                    Home <span className="sr-only">(current)</span>
-                  </a>
-                </li>
-
-                <li className="nav-item inactive">
-                  <a className="nav-link" href="about.html">
-                    About <span className="sr-only">(current)</span>
-                  </a>
-                </li>
-
-                <li className="nav-item inactive">
-                  <a className="nav-link" href="portfolio.html">
-                    Work <span className="sr-only">(current)</span>
-                  </a>
-                </li>
-
-                <li className="nav-item inactive">
-                  <a className="nav-link" href="contact.html">
-                    Contact <span className="sr-only">(current)</span>
-                  </a>
-                </li>
+                {pageNames.map((page) => renderNavTab(page))}
               </ul>
             </div>
           </div>
