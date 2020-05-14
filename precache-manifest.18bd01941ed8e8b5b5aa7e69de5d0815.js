6437de7da69af7a336b2d0ba249e278df57ef4c3
Sorrab <ramonpbarros@gmail.com>
2020-05-13 23:32:26 -0700

    Updates

---
+++ precache-manifest.18bd01941ed8e8b5b5aa7e69de5d0815.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9908f8fe67fd7819654bf037dbd1d2a3",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "badb07050de59c7bc17a",
+    "url": "/react-portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "df8d6b40a6bebc2bb4ac",
+    "url": "/react-portfolio/static/css/main.a5eb722f.chunk.css"
+  },
+  {
+    "revision": "badb07050de59c7bc17a",
+    "url": "/react-portfolio/static/js/2.6d69b2b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.6d69b2b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8d6b40a6bebc2bb4ac",
+    "url": "/react-portfolio/static/js/main.2a924713.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  },
+  {
+    "revision": "8be56dc01942d5661129aab6d9b33f81",
+    "url": "/react-portfolio/static/media/barhop-logo.8be56dc0.svg"
+  },
+  {
+    "revision": "51819d6757d1fbc8ce880e5aaf0c4031",
+    "url": "/react-portfolio/static/media/barhop.51819d67.png"
+  },
+  {
+    "revision": "db43273b9f6d2f75d8ab5a3237d6b977",
+    "url": "/react-portfolio/static/media/barhop.db43273b.gif"
+  },
+  {
+    "revision": "754095eccb30387990feccff7029fff8",
+    "url": "/react-portfolio/static/media/barhopfav.754095ec.png"
+  },
+  {
+    "revision": "c7865081a0b57476100c2ee2c2cb6e0f",
+    "url": "/react-portfolio/static/media/barhoproute.c7865081.png"
+  },
+  {
+    "revision": "c8b331e920319a0feded28233de7f9d2",
+    "url": "/react-portfolio/static/media/burger.c8b331e9.png"
+  },
+  {
+    "revision": "f646e16346ea37c3b2575677fbe86690",
+    "url": "/react-portfolio/static/media/burger.f646e163.gif"
+  },
+  {
+    "revision": "1ee9cedb6e1799fa4698e4acdcb7aaca",
+    "url": "/react-portfolio/static/media/codequiz.1ee9cedb.png"
+  },
+  {
+    "revision": "19ea80ead0773f1d4a7651e06e7c78f0",
+    "url": "/react-portfolio/static/media/employee.19ea80ea.png"
+  },
+  {
+    "revision": "6faa2310e8811fdf307447332444246c",
+    "url": "/react-portfolio/static/media/quiz.6faa2310.jpg"
+  },
+  {
+    "revision": "f1c2fd9d92a207a688de2c32ed25579e",
+    "url": "/react-portfolio/static/media/ramon.f1c2fd9d.png"
+  },
+  {
+    "revision": "dda396eea0be1d2fa415a3ebcbeaa1e0",
+    "url": "/react-portfolio/static/media/readmeTwo.dda396ee.png"
+  },
+  {
+    "revision": "dab6c7096c7077bf0f369e536b2871e0",
+    "url": "/react-portfolio/static/media/schedule.dab6c709.png"
+  },
+  {
+    "revision": "3ca8589e3cfff2acb5d0024bb58a9daf",
+    "url": "/react-portfolio/static/media/scheduler.3ca8589e.png"
+  },
+  {
+    "revision": "8b636004ef9c00b3e30e459c1931fa8a",
+    "url": "/react-portfolio/static/media/terminal.8b636004.svg"
+  },
+  {
+    "revision": "80cbbe394482fa2258a7c0e551d2ea24",
+    "url": "/react-portfolio/static/media/weather.80cbbe39.png"
+  },
+  {
+    "revision": "13f89f4db97ab61c6897adbc43c395d6",
+    "url": "/react-portfolio/static/media/weather_logo.13f89f4d.jpg"
+  }
+]);
